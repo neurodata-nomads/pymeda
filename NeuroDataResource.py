60456dd8acab4cc9e23c27b7cbb475ed810175aa
j1c <jaewonc78@gmail.com>
2017-12-20 02:57:13 -0500

    synaptome

---
+++ NeuroDataResource.py
@@ -0,0 +1,135 @@
+import numpy as np
+import intern
+from intern.remote.boss import BossRemote
+from intern.resource.boss.resource import ChannelResource, ExperimentResource, CoordinateFrameResource
+
+class NeuroDataResource:
+    def __init__(self, host, token, collection, experiment):
+        self._bossRemote = BossRemote({'protocol': 'https',
+                                       'host': host,
+                                       'token': token})
+        self.collection = collection
+        self.experiment = experiment
+        self.channels = self._bossRemote.list_channels(collection, experiment)
+        self.channels.remove('empty') #Delete "empty" channel
+        self._get_coord_frame_details()
+
+
+    def _get_coord_frame_details(self):
+        exp_resource = ExperimentResource(self.experiment, self.collection)
+        coord_frame = self._bossRemote.get_project(exp_resource).coord_frame
+
+        coord_frame_resource = CoordinateFrameResource(coord_frame)
+        data = self._bossRemote.get_project(coord_frame_resource)
+
+        self.max_dimensions = (data.z_stop, data.y_stop, data.x_stop)
+        self.voxel_size = (data.z_voxel_size, data.y_voxel_size, data.x_voxel_size)
+
+
+    def _get_channel(self, chan_name):
+        """
+        Helper that gets a fully initialized ChannelResource for an *existing* channel.
+        Args:
+            chan_name (str): Name of channel.
+            coll_name (str): Name of channel's collection.
+            exp_name (str): Name of channel's experiment.
+        Returns:
+            (intern.resource.boss.ChannelResource)
+        """
+        chan = ChannelResource(chan_name, self.collection, self.experiment)
+        return self._bossRemote.get_project(chan)
+
+    def assert_channel_exists(self, channel):
+        return channel in self.channels
+
+    def get_cutout(self, chan, zRange=None, yRange=None, xRange=None):
+        if chan not in self.channels:
+            print('Error: Channel Not Found in this Resource')
+            return
+        if zRange is None or yRange is None or xRange is None:
+            print('Error: You must supply zRange, yRange, xRange kwargs in list format')
+            return
+
+        channel_resource = self._get_channel(chan)
+        datatype = channel_resource.datatype
+
+        data = self._bossRemote.get_cutout(channel_resource,
+                                           0,
+                                           xRange,
+                                           yRange,
+                                           zRange)
+
+        return data
+
+    @staticmethod
+    def ingest_volume(host, token, channel_name, collection, experiment, volume):
+        """
+        Assumes the collection and experiment exists in BOSS.
+        """
+        
+        remote = BossRemote({'protocol': 'https',
+                             'host': host,
+                             'token': token})
+
+        if volume.dtype == 'uint64':
+            dtype = 'uint64'
+            img_type = 'annotation'
+            sources = ['empty']
+        else:
+            dtype = volume.dtype.name
+            img_type = 'image'
+            sources = []
+        
+        try:
+            channel_resource = ChannelResource(channel_name, collection, experiment)
+            channel = remote.get_project(channel_resource)
+        except:
+            channel_resource = ChannelResource(channel_name, collection, experiment,
+                                               type=img_type,
+                                               sources=sources,
+                                               datatype=dtype)
+            channel = remote.create_project(channel_resource)
+
+        #Get max size of experiment
+        exp_resource = ExperimentResource(experiment, collection)
+        coord_frame = remote.get_project(exp_resource).coord_frame
+        coord_frame_resource = CoordinateFrameResource(coord_frame)
+        data = remote.get_project(coord_frame_resource)
+        y_stop, x_stop = data.y_stop, data.x_stop
+        
+        for z in range(volume.shape[0]):
+            print('Uploading {} slice'.format(z))
+            remote.create_cutout(channel,
+                                 0,
+                                 (0, x_stop), 
+                                 (0, y_stop),
+                                 (z, z + 1), 
+                                 volume[z, :, :].reshape((-1, y_stop, x_stop)))
+
+        
+
+"""
+def test_function():
+    #load ground truth file
+    gt = io.imread('./data/gt.tiff')
+
+    #api token is stored externally for security
+    token = pickle.load(open('./data/token.pkl', 'rb'))
+    host = 'api.boss.neurodata.io'
+    myResource = NeuroDataResource(host,
+                                  token,
+                                  'collman',
+                                  'collman15v2',
+                                  [{'name':'annotation', 'dtype':'uint64'},
+                                   {'name':'DAPI1st', 'dtype':'uint8'},
+                                   {'name':'GABA488', 'dtype':'uint8'}])
+
+    cutout = myResource.get_cutout('annotation', [2,4], [2300,2500], [3400,3700])
+
+    print(gt.shape == cutout.shape)
+    print(delta_epsilon(np.mean(gt), np.mean(cutout), 1e-4))
+
+
+if __name__ == '__main__':
+    test_function()
+"""
---
+++ README.md
@@ -0,0 +1,49 @@
+# Synaptome
+Running MEDA on Synapses
+
+### System Requirements
+The software has been tested macOS Sierra 10.12.6 (2.9GHz Intel Core i5).
+
+#### Python depedencies:
+colorama --- 0.3.9<br/>
+scikit_image --- 0.13.1<br/>
+scipy --- 1.0.0<br/>
+numpy --- 1.13.1<br/>
+requests --- 2.18.4<br/>
+intern --- 0.9.4<br/>
+tifffile --- 0.12.1<br/>
+tqdm --- 4.19.5<br/>
+matplotlib --- 2.1.0<br/>
+progressbar2 --- 3.34.3<br/>
+scikit_learn --- 0.19.1<br/>
+pyfiglet --- 0.7.5<br/>
+
+### Installation Guide
+This assumes you have these tools already installed:
+
+1. Python 3.6 ([Download](https://www.python.org/downloads/))
+2. Git ([Installation](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git))
+2. Virtualenv ([Installation and usage](https://help.dreamhost.com/hc/en-us/articles/115000695551-Installing-and-using-Python-s-virtualenv-using-Python-3))
+3. Virtualenvwrapper ([Installation](http://virtualenvwrapper.readthedocs.io/en/latest/install.html))
+
+If you do not have these tools installed, you can click the links to download and install them. If you do have these tools installed, you can go ahead with the following. It should take about 2-3 minutes.
+1. Clone this repository
+    `git clone https://github.com/j1c/synaptome.git`
+2. Navigate to the repository directory
+    `cd synaptome`
+3. Create a virtualenvironment called synaptome
+    `mkvirtualenv -p python3 synaptome`
+2. Install the dependencies using pip and requirements.txt
+    `pip install -r requirements.txt`
+3. Make the synaptome virtual environment available in jupyter notebook.
+    `python -m ipykernel install --user --name=synaptome`
+4. Run jupyter notebook
+    `jupyter notebook`
+
+### Demo
+Open the `api_key.txt` text file in the repository, and copy/paste your token into the file. This is required for grabbing data from BOSS, and later, uploading data back to the BOSS.
+
+1. Navigate to the jupyter tab in your browser. If you closed it by accident, you can reopen it using the following url:
+    `localhost:8888/tree`
+2. Open the `demo.ipynb`
+3. You can run each cell to view the results.
